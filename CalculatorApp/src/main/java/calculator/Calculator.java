89a17f62dd7155d045278948111456443609ceef
angeloblanchard <ablanchard3@mail.greenriver.edu>
2017-10-14 20:46:36 -0700

    Calculator is fully functional in regards to the displayed buttons in the app(addition, subtraction, multiplication, division). Can also continue to compute after hitting enter.

--- CalculatorApp/src/main/java/calculator/Calculator.java
+++ CalculatorApp/src/main/java/calculator/Calculator.java
@@ -8,9 +8,11 @@ import javafx.scene.input.KeyEvent;
 
 public class Calculator
 {
-    private static String operandOneString;
-    private static String operandTwoString;
-    private static String operator;
+    private static String operandOneString = "";
+    private static String operandTwoString = "";
+    private static String operator = "";
+    private static boolean flagEnter;
+    private static boolean flagOperator;
 //    private EventHandler<KeyEvent> buttonHandler = new EventHandler<KeyEvent>()
 //    {
 //        @Override
@@ -25,15 +27,15 @@ public class Calculator
     private static int calculate()
     {
         int answer = 0;
-        int operand1 = refactorToNum(operandOneString);
-        int operand2 = refactorToNum(operandTwoString);
+        int operand1 = Integer.parseInt(operandOneString);
+        int operand2 = Integer.parseInt(operandTwoString);
 
         switch (operator) {
             case "+" : answer = operand1 + operand2;
             break;
             case "-" : answer = operand1 - operand2;
             break;
-            case "*" : answer = operand1 - operand2;
+            case "*" : answer = operand1 * operand2;
             break;
             case "/" : if (operand2 == 0)
             {
@@ -63,29 +65,65 @@ public class Calculator
                     {
                         output.setText("");
                     }
-                    boolean flagEnter = false;
-                    boolean flagOperator = false;
-                    switch (key) {
+                    switch (key)
+                    {
                         case "0" : case "1" : case "2" : case "3" : case "4" : case "5" :
                         case "6" : case "7" : case "8" : case "9" :
-                            operandOneString += key;
+                            if (flagOperator)
+                            {
+                                if (operandTwoString.isEmpty())
+                                {
+                                    operandTwoString = key;
+                                }
+                                else
+                                {
+                                    operandTwoString += key;
+                                }
+                            }
+                            else
+                            {
+                                if (operandOneString.isEmpty())
+                                {
+                                    operandOneString = key;
+                                }
+                                else
+                                {
+                                    operandOneString += key;
+                                }
+                            }
+                            flagEnter = false;
                         break;
 
                         case "+" : case "-" : case "*" : case "/" :
                             operator = key;
                             flagOperator = true;
+                            flagEnter = false;
                             break;
-
-                        case "Enter" : flagEnter = true;
+                        case "Enter" :
+                            if (operandTwoString.isEmpty())
+                            {
+                                flagEnter = false;
+                                break;
+                            }
+                            else
+                            {
+                                flagEnter = true;
+                            }
                         break;
+
+                        default :
+                            flagEnter = false;
+                            flagOperator = false;
                     }
+
                     if (flagEnter)
                     {
                         int answer = calculate();
-                        output.setText("error2");
                         output.setText(String.valueOf(answer));
+                        operandTwoString = "";
+                        operandOneString = String.valueOf(answer);
                     }
-                    else if (flagOperator)
+                    else if (flagOperator && operandTwoString.isEmpty())
                     {
                         output.setText("");
                     }
@@ -97,12 +135,4 @@ public class Calculator
             });
         }
     }
-
-
-
-    private static int refactorToNum(String number)
-    {
-        return Integer.parseInt(number);
-    }
-
 }
