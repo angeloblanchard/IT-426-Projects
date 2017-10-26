c3e3c8df37e66f89f6c83b9647a2a2fa1c5af42e
angeloblanchard <ablanchard3@mail.greenriver.edu>
2017-10-25 22:47:18 -0700

    Have gotten OriginalKWIC working to circularly shift, accepting an input file.

--- KWICProgram/src/main/java/kwic/AlternateKWIC.java
+++ KWICProgram/src/main/java/kwic/AlternateKWIC.java
@@ -1,12 +1,19 @@
 package kwic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class AlternateKWIC
 {
     public static void main(String[] args)
     {
-
+        KWICLines lines = new KWICLines();
+        lines.circularShift();
+        lines.outputToFile();
     }
 
     public String getFileName()
@@ -15,23 +22,51 @@ public class AlternateKWIC
     }
 
 
-    private class KWICLines
+    private static class KWICLines
     {
-        private ArrayList<String> lines;
+        private ArrayList<String> lines = new ArrayList<String>();
+        private KWICLines()
+        {
+            File filename = new File("input1.txt");
 
+            Scanner lineScan = new Scanner("input1.txt");
+
+            while(lineScan.hasNextLine())
+            {
+                lines.add(lineScan.nextLine());
+            }
+            lineScan.close();
+        }
         public void circularShift()
         {
+            for (int i = 0; i < lines.size(); i++)
+            {
+                lines.set(i, shiftLine(lines.get(i)));
+            }
+            Collections.sort(lines);
+        }
 
+        private String shiftLine(String line)
+        {
+            String[] array = line.split(" ", 2);
+            String newLine = array[1] + " " + array[0];
+            return newLine;
         }
 
         public void outputToFile()
         {
-
+            try {
+                PrintStream fileOutput = new PrintStream(new File("output1.txt"));
+                System.out.println("New shifted lines:\n");
+                for(String line : lines)
+                {
+                    fileOutput.println(line);
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Failed to create file.");;
+            }
         }
 
-        private void sortLines()
-        {
-
-        }
     }
 }
--- KWICProgram/src/main/java/kwic/OriginalKWIC.java
+++ KWICProgram/src/main/java/kwic/OriginalKWIC.java
@@ -1,32 +1,90 @@
 package kwic;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class OriginalKWIC
 {
     public static void main(String[] args)
     {
+        ArrayList<String> lines;
+        File filename = new File(getInputFile());
+        lines = readLines(filename);
+        System.out.println("File found!");
+        circularShift(lines);
+        sortLines(lines);
+
+        outputToFile(lines);
+    }
+
+    public static String getInputFile()
+    {
+        System.out.println("Input file name?");
+        Scanner console = new Scanner(System.in);
+        String filename = console.next();
+        console.close();
+        return filename;
+    }
+
+    public static ArrayList<String> readLines(File file)
+    {
+        ArrayList<String> lines = new ArrayList<String>();
+        try {
+            Scanner lineScan = new Scanner(file);
+            while(lineScan.hasNextLine())
+            {
+                lines.add(lineScan.nextLine());
+            }
+            lineScan.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return lines;
 
     }
 
-    public File getInputFile(String inputFilename)
+    public static void circularShift(ArrayList<String> lines)
     {
-        return null;
+        for (int i = 0; i < lines.size(); i++)
+        {
+            lines.set(i, shiftLine(lines.get(i)));
+        }
     }
 
-    public String circularShift(ArrayList lines)
+    private static String shiftLine(String line)
     {
-        return null;
+        String[] array = line.split(" ", 2);
+        String newLine = array[1] + " " + array[0];
+        return newLine;
     }
 
-    public String sortLines(String lines)
+    public static void sortLines(ArrayList<String> lines)
     {
-        return null;
+        Collections.sort(lines);
     }
 
-    public void outputToFile(String lines)
+    public static void outputToFile(ArrayList<String> lines)
     {
-
+        try {
+            PrintStream fileOutput = new PrintStream(new File("output.txt"));
+            System.out.println("New shifted lines:\n");
+            for(String line : lines)
+            {
+                fileOutput.println(line);
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
