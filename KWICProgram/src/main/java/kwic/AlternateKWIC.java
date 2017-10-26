2b686897298784102932ad9c64f22098881b2c4d
angeloblanchard <ablanchard3@mail.greenriver.edu>
2017-10-25 21:00:18 -0700

    Added Original and Alternate KWIC starting structures.

---
+++ KWICProgram/src/main/java/kwic/AlternateKWIC.java
@@ -0,0 +1,37 @@
+package kwic;
+
+import java.util.ArrayList;
+
+public class AlternateKWIC
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public String getFileName()
+    {
+        return null;
+    }
+
+
+    private class KWICLines
+    {
+        private ArrayList<String> lines;
+
+        public void circularShift()
+        {
+
+        }
+
+        public void outputToFile()
+        {
+
+        }
+
+        private void sortLines()
+        {
+
+        }
+    }
+}
---
+++ KWICProgram/src/main/java/kwic/OriginalKWIC.java
@@ -0,0 +1,32 @@
+package kwic;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class OriginalKWIC
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public File getInputFile(String inputFilename)
+    {
+        return null;
+    }
+
+    public String circularShift(ArrayList lines)
+    {
+        return null;
+    }
+
+    public String sortLines(String lines)
+    {
+        return null;
+    }
+
+    public void outputToFile(String lines)
+    {
+
+    }
+}
